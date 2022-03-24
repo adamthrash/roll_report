51b16451c2edd61c1c0dbe116f18ff0f1d143e3d
Adam Thrash <cathrash92@gmail.com>
2022-03-24 16:15:14 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// import table from './report.json';
+// import Alpine from 'alpinejs';
+// window.Alpine = Alpine;
+
+// Alpine.store('rows', () => {return [...table]})
+// Alpine.store('columns', () => Object.keys(table[0]))
+// Alpine.start();
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  content: ["./src/*.{html,js,pug}"],
+  theme: {
+    extend: {
+      colors: {
+        'msstate': {
+          DEFAULT: '#5D1725',
+          '50': '#F5D7DD',
+          '100': '#EEB8C3',
+          '200': '#DE7B8F',
+          '300': '#CF3E5B',
+          '400': '#9A263D',
+          '500': '#5D1725',
+          '600': '#511420',
+          '700': '#44111B',
+          '800': '#380E16',
+          '900': '#2C0B11'
+        }
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+};
